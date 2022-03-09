7858a70d07057b9a794324110b216f0fc2447bd3
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-03-09 23:43:12 +0530

    Excersise 1 by of taking userinput in Ditionary

--- Exercise1.py
+++ Exercise1.py
@@ -1,6 +1,6 @@
-print('Welcome to Owais Dictionary')
-Dict  = {'abase':'Cause to feel shame','benefit':'Advantage','callow':'Young and inExperience','Dally':'Wastetime','Dictionary':' A dictionary is an unordered and mutable Python container that stores mappings of unique keys to values.'}
+print('**** Welcome to Owais Dictionary *****')
+myDict  = {'abase':'Cause to feel shame','benefit':'Advantage','callow':'Young and inExperience','Dally':'Wastetime','Dictionary':' A dictionary is an unordered and mutable Python container that stores mappings of unique keys to values.','List':'https://www.w3schools.com/python/python_lists.asp'}
 print('The required meaning is :')
 Search = input()
-print(Dict[Search])
+print(myDict[Search])
 print('Thanks for Using My Dictionary')
---
+++ rough.py
@@ -0,0 +1,11 @@
+s = set()
+# print(type(s))
+# l = [1,2,3,4,5,6]
+# s_from_list = set(l)
+# print(s_from_list)
+# print(type(s_from_list))
+s.add(22)
+s.add(23)
+s1 = {4,5,0,22,6}
+print(s.isdisjoint(s1)) # Kya dono Bhi Elements same hai kya
+print(max(s1))
