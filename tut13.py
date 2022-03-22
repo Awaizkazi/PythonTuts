f8b2495f90c854acbf3ceecc84e6c6d1dca06b58
Awaizkazi <88947135+Awaizkazi@users.noreply.github.com>
2022-03-23 00:07:07 +0530

    List Infromation But Not in Detail

---
+++ tut13.py
@@ -0,0 +1,19 @@
+# var1 = 6
+# var2 = 56
+# print('Enter a Number :')
+# var3 = int(input())  # var3 bhi 1 integer ban gya
+#
+# if var3 >=var2:
+#     print('Greater') # print ke pehle jo bhi hai usko hmm indentation bolte wo 1 tara se automatically tab jitna Space leleta hai
+# else:
+#     print('Lesser')
+
+
+print('Enter your Age :')
+userInput = int(input())
+if userInput >18:
+    print('Your are Eligible for Drive')
+elif userInput ==18:
+    print('Not Decided yet , Come Physically')
+else :
+    print('You are not Eligible for Driving')
